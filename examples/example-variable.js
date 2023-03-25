cd90a4d8cd6eef7ba207e65ec5fba192cbc570ab
monstermichl <vouillarmet.michel@gmail.com>
2023-03-25 17:40:41 +0100

    Update variable example

--- examples/example-variable.js
+++ examples/example-variable.js
@@ -10,18 +10,30 @@ const stringVariable = new StringVariable('string');
 const numberVariable = new NumberVariable('number');
 
 const script = new Script([
-    stringVariable.set('"Start here"'),
-    numberVariable.set(0),
+    stringVariable.set(),  /* Initialize string variable. */
+    numberVariable.set(0), /* Initialze the number variable. */
 
+    /* Loop while number variable is less than 5. */
     new While(numberVariable.isLess(5), [
-        new If(stringVariable.isEqual('Start here'), [
-            'echo "Hello World"',
-            stringVariable.set('Changed value'),
+
+        /* If string variable is empty, set it to 'Hello'. */
+        new If(stringVariable.isEqual(), [
+            stringVariable.set('Hello'),
+
+        /* If number variable is 1, append ' again' to string variable. */
+        ]).elseIf(numberVariable.isEqual(1), [
+            stringVariable.set(stringVariable.append(' again')),
+
+        /* Ever other time, append ' and again' to the string variable. */
         ]).else([
-            `echo ${numberVariable.value}`,
+            stringVariable.set(stringVariable.append(' and again')),
         ]),
+        /* Print the string variable to console. */
+        `echo "${stringVariable.value}"`,
+
+        /* Increment the number variable by 1. */
         numberVariable.set(numberVariable.increment()),
-    ]).setComment('Loop while number variable is less than 5'),
+    ]),
 ]).dump();
 
 console.log(script);
