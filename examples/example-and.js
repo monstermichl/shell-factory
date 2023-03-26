a6724a48c1551a3c197f319fb6555e6a2e9b2159
monstermichl <vouillarmet.michel@gmail.com>
2023-03-26 16:08:32 +0200

    Add and- and or-example

---
+++ examples/example-and.js
@@ -0,0 +1,17 @@
+import {
+    Script,
+    Command,
+    If
+} from '../dist/index.mjs';
+
+const script = new Script([
+    new Command('echo "File content"')
+        .pipe('grep -o -e "content"')
+        .and('echo "ok"'),
+
+    new If('1 -eq 1').and('2 -eq 2').addContent([
+        'echo "What a useless comparison"',
+    ]),
+]).dump();
+
+console.log(script);
