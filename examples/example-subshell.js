9d74d0fd5606bfd3c76d29f8c51a8da5ddee2e73
monstermichl <vouillarmet.michel@gmail.com>
2023-03-31 19:24:04 +0200

    Add subshell example

---
+++ examples/example-subshell.js
@@ -0,0 +1,24 @@
+import {
+    Script,
+    If,
+    StringVariable,
+    Subshell,
+} from '../dist/index.mjs';
+
+const variable = new StringVariable('response');
+const script = new Script([
+    'read -p "What\'s your name? " name',
+    variable.set(
+        new If('"$name" != ""', [
+            'echo "Hello $name"',
+        ]).eval(),
+    ),
+
+    new If(variable.isEmpty, [
+        variable.set(Subshell.eval('echo "I don\'t understand"')),
+    ]),
+
+    `echo "${variable.value}"`,
+]).dump();
+
+console.log(script);
